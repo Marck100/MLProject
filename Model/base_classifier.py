081cd01f46bcf4efd711535c7dfe0efffc76f9d6
annasetzu <annasetzu03@gmail.com>
2023-03-06 10:58:24 +0100

    base_classifier.py - added comments

--- Model/base_classifier.py
+++ Model/base_classifier.py
@@ -1,14 +1,16 @@
+# Needed imports
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 from Model.csv_loader import CSVLoader
 import matplotlib.pyplot as plt
 
 
+# Inherit CSVLoader
 class BaseClassifier(CSVLoader):
     _classifier = None
     _prepared = False
 
-    # Split features from classes
+    # Split features and classes
     def prepareSet(self):
         df = self._df
 
@@ -19,11 +21,15 @@ class BaseClassifier(CSVLoader):
 
     def splitSet(self, random_state=0, test_size=0.25):
         x, y = self.prepareSet()
+        # TODO Remove random_state (??)
+        # Split dataset into validation and training set
         return train_test_split(x, y, random_state=random_state, test_size=test_size)
 
+    # Init _classifier variable
     def initClassifier(self):
         pass
 
+    # Prepare classifier (load dataset, initialize classifier, fit classifier)
     def prepare(self):
         if not self._prepared:
             self.load()
@@ -32,16 +38,20 @@ class BaseClassifier(CSVLoader):
 
             self._prepared = True
 
+    # Show classifier parameters
     def showParameters(self):
         print(self._classifier.get_params())
 
+    # Return better classifier (based on parameters tuning)
     def tuneParameters(self):
         pass
 
+    # Fit classifier with training set
     def fitClassifier(self):
         train_x, _, train_y, _ = self.splitSet()
         self._classifier.fit(train_x, train_y)
 
+    # Predict labels using validation set
     def predict(self):
         _, test_x, _, _ = self.splitSet()
         pred_y = self._classifier.predict(test_x)
@@ -65,6 +75,7 @@ class BaseClassifier(CSVLoader):
 
         return wrong / len(pred_y)
 
+    # Display confusion matrix
     def showConfusionMatrix(self):
         _, _, _, test_y = self.splitSet()
         prediction = self.predict()
@@ -90,6 +101,7 @@ class BaseClassifier(CSVLoader):
 
         self.showConfusionMatrix()
 
+    # Compute accuracy on validation set (see comparator.py)
     def validationSetAccuracy(self):
         prediction = self.predict()
         _, _, _, test_y = self.splitSet()
