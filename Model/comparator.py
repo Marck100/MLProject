1f73f32611306291fc1c771f40ebd9ccc8d9b8b2
Marck100 <marcopilloni@icloud.com>
2023-03-06 19:01:19 +0100

    Added comparator comments

--- Model/comparator.py
+++ Model/comparator.py
@@ -1,19 +1,24 @@
+# Needed imports
 from Model.base_classifier import BaseClassifier
 from Model.decision_tree_classifier import CDecisionTreeClassifier
 from Model.nearest_neighbor_classifier import CNearestNeighborClassifier
 import matplotlib.pyplot as plt
 
 
+# Compare classifiers (accuracy on validation set)
 class Comparator:
 
+    # Dictionary containing classifiers divided by preprocessing methods (see main)
     _classifiers: {str: [BaseClassifier]}
 
     def __init__(self, classifiers: {str: [BaseClassifier]}):
         self._classifiers = classifiers
 
+    # Compare classifiers and show results in a plot
     def compareClassifiers(self):
         classifiers = self._classifiers
         n_groups = len(classifiers)
+        # Subplots to show more charts in the same view
         fig, axs = plt.subplots(n_groups)
 
         for index, key in enumerate(classifiers):
@@ -21,18 +26,18 @@ class Comparator:
             for classifier in classifier_group:
                 classifier.prepare()
 
+            # Retrieve class names
             classifiers_name = list(map(lambda x: x.__class__.__name__, classifier_group))
-
+            # Compute accuracy for every classifier
             validation_set_accuracies = list(map(lambda x: x.validationSetAccuracy(), classifier_group))
 
+            # Display results
             axs[index].barh(classifiers_name, validation_set_accuracies)
             axs[index].title.set_text(key)
 
+        # Title + layout fixes
         fig.suptitle('Validation set scores')
-
-        #fig.set_size_inches(12, 20, forward=True)
         fig.set_size_inches(20, 20, forward=True)
-
         plt.subplots_adjust(hspace=1)
         plt.show()
 
